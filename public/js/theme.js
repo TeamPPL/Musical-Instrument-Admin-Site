4c684992b2e0c8ef4094f73e280e07f8bb4ccea7
Phu Do <dophu1402@gmail.com>
2020-12-08 07:41:05 +0700

    preview image

--- public/js/theme.js
+++ public/js/theme.js
@@ -704,3 +704,19 @@ $('#selectedFile').change(function () {
     var a = $('#selectedFile').val().toString().split('\\');
     $('#fakeInput').val(a[a.length -1]);
 });
+
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
